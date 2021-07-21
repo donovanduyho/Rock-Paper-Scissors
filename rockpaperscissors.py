f6566ca18b9846eec70d56feb6a515d13999b646
Donovan Ho <donovanduyho@gmail.com>
2021-07-21 11:51:06 -0400

    Create rockpaperscissors.py
    Created and added layout of program.

---
+++ rockpaperscissors.py
@@ -0,0 +1,10 @@
+# Create variables for each object (Rock, Paper, Scissors). 
+# Assign a unique number for variables "rock", "paper", "scissors". 
+# Create a method that takes an input of the user and assign it to "player".
+# Use a random number generator to select the object for the computer/opponent and assign it to variable "computerOpponent".
+# Use conditional statements to compare the two variables in order to determine an outcome.
+# If the variables are equal, the game results in a draw. If the variables are different, the program determines the winner. 
+# Create a variable to keep the score. First player to reach 2 wins out of 3 tries is the victor. 
+# Ask user if they would like to run again.
+# Maybe create a UI to display graphics? 
+
