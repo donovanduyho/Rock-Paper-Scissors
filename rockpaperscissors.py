f4a076c057b10b32223498b57b3da500409742eb
Donovan Ho <donovanduyho@gmail.com>
2021-07-21 16:12:37 -0400

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,10 +1,90 @@
-# Create variables for each object (Rock, Paper, Scissors). 
-# Assign a unique number for variables "rock", "paper", "scissors". 
-# Create a method that takes an input of the user and assign it to "player".
-# Use a random number generator to select the object for the computer/opponent and assign it to variable "computerOpponent".
-# Use conditional statements to compare the two variables in order to determine an outcome.
-# If the variables are equal, the game results in a draw. If the variables are different, the program determines the winner. 
-# Create a variable to keep the score. First player to reach 2 wins out of 3 tries is the victor. 
-# Ask user if they would like to run again.
-# Maybe create a UI to display graphics? 
+import random #For randomized choice
+import timeit #For timer 
+import math #For rounding to nearest number 
 
+#Records time of execution
+start = timeit.default_timer() 
+
+#Tracks total amount of rounds played throughout duration of program
+totalRounds = 0 
+
+#Loops until the user refuses to play again
+while True: 
+
+    #Both score trackers stay within the loop to track how many wins in a Bo3 there are
+    playerWins = 0 
+    computerWins = 0
+
+    #Continues the Bo3 until either the player wins or computer wins by getting to 2 first 
+    while(playerWins < 2 and computerWins < 2): 
+        possibleMoves = ["R", "P", "S"]
+        computer = random.choice(possibleMoves) #Computer randomly selects an option from possibleMoves
+
+        #True until correct input is given
+        while True: 
+            print("SELECT ACTION BELOW:")
+            player = input("(R) ROCK | (P) PAPER | (S) SCISSORS: ")
+            player = player.upper() #Capitalize user input
+            print("")
+            if(player not in possibleMoves): #Test if input is within database 
+                print("INVALID INPUT: SELECT R, P, OR S")
+                print("")
+            else:
+                break #Exit loop
+
+        print("YOUR CHOICE: " + player)
+        print("THE COMPUTER'S CHOICE: " + computer)
+
+        #Determines a winner through conditional statements
+        if(player == computer): 
+            print("DRAW")
+        elif(player == "R"): 
+            if(computer == "S"): 
+                print("ROCK > SCISSORS. YOU WIN.")
+                playerWins = playerWins + 1
+            else: 
+                print("PAPER > ROCK. THE COMPUTER WINS.")
+                computerWins = computerWins + 1
+        elif(player == "P"): 
+            if(computer == "R"): 
+                print("PAPER > ROCK. YOU WIN.")
+                playerWins = playerWins + 1
+            else: 
+                print("SCISSORS > PAPER. THE COMPUTER WINS.")
+                computerWins = computerWins + 1
+        elif(player == "S"): 
+            if(computer == "P"): 
+                print("SCISSORS > PAPER. YOU WIN.")
+                playerWins = playerWins + 1
+            else: 
+                print("ROCK > SCISSORS. THE COMPUTER WINS.")
+                computerWins = computerWins + 1
+        
+        #Increments by 1 after every completed round
+        totalRounds = totalRounds + 1
+
+        print("")
+        print("YOUR WINS: ", playerWins)
+        print("COMPUTER WINS: ", computerWins)
+
+        #If player or computer reaches two, display message and exit game
+        if(playerWins == 2):
+            print("YOU HAVE BEATEN THE COMPUTER.")
+        elif(computerWins ==2):
+            print("THE COMPUTER HAS BEATEN YOU.")
+        
+        print("")
+        
+    #Asks user to play again
+    runAgain = input("PLAY AGAIN? (Y/N): ")
+    runAgain = runAgain.upper()
+    if(runAgain != "Y"):
+        print("TOTAL ROUNDS PLAYED: ", totalRounds) #Displays total rounds throughout the entire duration
+        break #Exit program
+
+
+stop = timeit.default_timer() #Records the time when the program concluded.
+print("[ Approximate Execution Time (Seconds): ", math.trunc(stop - start), " ]") #Subtract start from stop, round to nearest second
+                                                                                    
+
+ 
